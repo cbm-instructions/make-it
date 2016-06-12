eea0c191681eb16e0dabe04a882d57d44a3ad68f
Patrick Fruh <info@kaeltis.de>
2016-06-12 17:10:37 +0200

    new code for IR

--- src/arduino/LEDTreppe/LEDTreppe.ino
+++ src/arduino/LEDTreppe/LEDTreppe.ino
@@ -1,41 +1,42 @@
-#include <NewPing.h>
+#include <IRremote.h>
 #include <Adafruit_NeoPixel.h>
 
-#define TRIGGER_PIN   12 // Arduino pin tied to trigger pin on ping sensor.
-#define ECHO_PIN      11 // Arduino pin tied to echo pin on ping sensor.
-#define MAX_DISTANCE  250 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define PIN_IR 3
+#define PIN_DETECT 2
 
-#define NUMPIXELS      119
+#define NUMPIXELS      240
 #define PIXEL_PIN      6
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+IRsend irsend;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+void setup()
+{
+  pinMode(PIN_DETECT, INPUT);
+  irsend.enableIROut(38);
+  irsend.mark(0);
+
   pixels.begin();
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  delay(50);
+  bool irstatus = !digitalRead(PIN_DETECT);
 
-  unsigned long uS = sonar.ping_median();
-  unsigned long cm = sonar.convert_cm(uS);
-
-  Serial.print("Ping: ");
-  Serial.print(cm); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
-
-  if (cm != 0) {
+  if (!irstatus) {
     enablePixels();
   } else {
     disablePixels();
   }
+
+  Serial.println(irstatus);
 }
+
 
 void enablePixels() {
   for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 0, 64));
+    pixels.setPixelColor(i, pixels.Color(255, 140, 0));
   }
   pixels.show();
 }
@@ -46,5 +47,3 @@ void disablePixels() {
   }
   pixels.show();
 }
-
-
